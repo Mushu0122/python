34728fcc7ad2a2c261415a2a80f52d4f8226d6ee
Mushu0122 <mushrafuddin427@gmail.com>
2024-03-04 03:13:24 -0600

    my string programming completed

---
+++ string.py
@@ -0,0 +1,53 @@
+"""string="hello world"
+print(type(string))
+print(string[-1])
+string1="HEllo"
+print(string.capitalize())
+print(string1.lower())
+print(string.upper())
+print(string.count('l'))
+print(string.index('e'))"""
+
+
+"""string="hello world"
+print(string)
+print(type(string))
+print(string[-1])
+print(string.capitalize())
+print(string.lower())
+print(string.upper())
+print(string.count('l'))
+print(string.index('e'))"""
+
+"""string="mushu"
+print(string)
+print(type(string))
+print(string[0])
+print(string.capitalize())
+print(string.lower())
+print(string.upper())
+print(string.count('m'))
+print(string.index('u'))"""
+
+"""a='08/02/20/24'
+print(a)
+print(a.replace('/',':'))
+print(a.split('/')) # ['08','02','20','24']
+print(a.split()) """
+
+"""print(string.split())
+print(string.islower())
+print(string.isupper())
+
+str="python language"
+b=str.split()
+print(b)"""
+
+
+a='08/02/20/24'
+print(a)
+print(a.replace('/',':'))
+print(a.split('/'))
+print(a.split())
+
+
